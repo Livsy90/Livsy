0f1d6ce52a9cc5b2b65847970b3ce15764816dfa
Artem Mirzabekian <artmir@live.ru>
2021-04-18 20:10:27 +0300

    Remove excess extension

--- Livsy/Utils/Extensions/UIView+adds.swift
+++ Livsy/Utils/Extensions/UIView+adds.swift
@@ -48,27 +48,3 @@ extension UIView {
     }
     
 }
-
-extension UIView {
-    func showAnimation(_ completionBlock: @escaping () -> Void) {
-        let generator = UIImpactFeedbackGenerator(style: .light)
-        generator.impactOccurred()
-        isUserInteractionEnabled = false
-        UIView.animate(withDuration: 0.1,
-                       delay: 0,
-                       options: .curveLinear,
-                       animations: { [weak self] in
-                        self?.transform = CGAffineTransform.init(scaleX: 0.85, y: 0.85)
-                       }) {  (done) in
-            UIView.animate(withDuration: 0.1,
-                           delay: 0,
-                           options: .curveLinear,
-                           animations: { [weak self] in
-                            self?.transform = CGAffineTransform.init(scaleX: 1, y: 1)
-                           }) { [weak self] (_) in
-                self?.isUserInteractionEnabled = true
-                completionBlock()
-            }
-                       }
-    }
-}
